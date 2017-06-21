92e424f78f97808b0e12d21dab1d6db8049c5a99
joelbarrette <joel.barrette@gmail.com>
2017-06-21 11:12:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,8 @@
 				
 				scene.clearColor = new BABYLON.Color3(1, 1, 1);
 				
-				
+				//Enables Skybox 
+				/*
 				var skybox = BABYLON.Mesh.CreateBox("skyBox", 1000.0, scene);
 				var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
 				skyboxMaterial.backFaceCulling = false;
@@ -26,7 +27,7 @@
 				skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
 				skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("textures/skybox", scene);
 				skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-				
+				*/
 				
 				return scene;
 			}
